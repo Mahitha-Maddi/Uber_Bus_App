4e2072ce25d0949a39b1efcc58cfa6e4a86be45c
sayalipathare <pathare.s@husky.neu.edu>
2021-04-22 22:09:01 -0400

    frontend changes and load testing test cases added for K6

--- uberfrontend/src/pages/Home.js
+++ uberfrontend/src/pages/Home.js
@@ -1,68 +1,199 @@
 import React, { useState, Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
+import { CardActionArea } from '@material-ui/core';
+import { CardContent } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
-import home from "../icons/home.jpeg"
+import home from "../icons/home.jpeg";
 
 import uber1 from "../styles/images/uber1.jpeg";
 import uber2 from "../styles/images/uber2.jpeg";
 import uber3 from "../styles/images/uber3.jpeg";
 import uber4 from "../styles/images/uber4.jpeg";
+import card1 from "../styles/images/card1.jpeg";
+import SF from "../styles/images/card2.jpeg";
+import Charleston from "../styles/images/Charleston.jpeg";
+import Chicago from "../styles/images/chicago.jpeg";
+import newOrleans from "../styles/images/new-orleans-hero.jpeg";
+import savanna from "../styles/images/savannah-georgia-SAVANNAH0519.jpeg"
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 //import '../styles/home.css';
-
-
 
 import "../styles/home.css";
 
 const styles = (theme) => ({
   root: {
-     display: "flex",
+    display: "flex",
     flexWrap: "wrap",
     justifyContent: "space-around",
     overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
-    marginTop: '100px'
-  }
-
+    marginTop: "100px",
+  },
+  
 });
+
+
 
 function Home(props) {
   const { classes } = props;
   const [checkout, setCheckOut] = useState(false);
+  
   return (
-
-      <div className={classes.root}>
-       
-      <div class='container-fluid'>
-
-            <Carousel fade>
-                <div>
-                <img className="d-block w-100" src={uber1} alt="First slide" />
-              <h3>Adding a Joy to your Journey</h3>
-                </div>
-                <div>
-                <img className="d-block w-100" src={uber2} alt="Second slide" />
-                <h3>Lets you Explore the Best</h3>
-                </div>
-                <div>
-                    <img className="d-block w-100" src={uber3} alt="Fourth slide"/>
-                    <h3>Bringing Joy From Every journey</h3>
-                </div>
-                <div>
-                    <img className="d-block w-100" src={uber4} alt="Fourth slide"/>
-                    <h3>Quality Travelling, Quality Service</h3>
-                </div>
-
-            </Carousel>
-            
-      
-        </div>
-        </div>
-
-
+    <div className={classes.root}>
+      <div class="container-fluid">
+        <Carousel fade>
+          <div>
+            <img className="d-block w-100" src={uber1} alt="First slide" />
+            <h3>Adding a Joy to your Journey</h3>
+          </div>
+          <div>
+            <img className="d-block w-100" src={uber2} alt="Second slide" />
+            <h3>Lets you Explore the Best</h3>
+          </div>
+          <div>
+            <img className="d-block w-100" src={uber3} alt="Fourth slide" />
+            <h3>Bringing Joy From Every journey</h3>
+          </div>
+          <div>
+            <img className="d-block w-100" src={uber4} alt="Fourth slide" />
+            <h3>Quality Travelling, Quality Service</h3>
+          </div>
+        </Carousel>
+        <CardDeck>
+          <Card >
+          <CardActionArea href="https://www.planetware.com/tourist-attractions-/new-york-city-us-ny-nyc.htm">
+          <CardContent>
+            <Card.Img variant="top" src={card1} width='100px' height='160px' />
+            <Card.Body>
+              <Card.Title>New York</Card.Title>
+              <Card.Text>
+              New York is a city made up of five boroughs: 
+              Manhattan, Brooklyn, Queens, the Bronx, and Staten Island. 
+              Most tourist attractions are in Manhattan, 
+              with a few scattered across Brooklyn, Queens, and the Bronx. 
+              
+              </Card.Text>
+              <Typography>Know More!</Typography>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card >
+          <CardActionArea href="https://www.planetware.com/tourist-attractions-/new-york-city-us-ny-nyc.htm">
+          <CardContent>
+            <Card.Img variant="top" src={Charleston} width='100px' height='160px' />
+            <Card.Body>
+              <Card.Title>New York</Card.Title>
+              <Card.Text>
+              New York is a city made up of five boroughs: 
+              Manhattan, Brooklyn, Queens, the Bronx, and Staten Island. 
+              Most tourist attractions are in Manhattan, 
+              with a few scattered across Brooklyn, Queens, and the Bronx. 
+              
+              </Card.Text>
+              <Typography>Know More!</Typography>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card >
+          <CardActionArea href="https://www.travelandleisure.com/travel-guide/san-francisco">
+          <CardContent>
+            <Card.Img variant="top" src={SF} width='100px' height='160px' />
+            <Card.Body>
+              <Card.Title>San Francisco</Card.Title>
+              <Card.Text>
+              San Francisco is home to Golden Gate Park, 
+              which is the third most visited park in the U.S. 
+              and spans 1,017 acres. 
+              </Card.Text>
+              <Typography>Know More!</Typography>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+        </CardDeck>
+        <hr></hr>
+        <CardDeck>
+          <Card >
+          <CardActionArea href="https://www.planetware.com/tourist-attractions-/new-york-city-us-ny-nyc.htm">
+          <CardContent>
+            <Card.Img variant="top" src={card1} width='100px' height='160px' />
+            <Card.Body>
+              <Card.Title>New York</Card.Title>
+              <Card.Text>
+              New York is a city made up of five boroughs: 
+              Manhattan, Brooklyn, Queens, the Bronx, and Staten Island. 
+              Most tourist attractions are in Manhattan, 
+              with a few scattered across Brooklyn, Queens, and the Bronx. 
+              
+              </Card.Text>
+              <Typography>Know More!</Typography>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card >
+          <CardActionArea href="https://www.planetware.com/tourist-attractions-/new-york-city-us-ny-nyc.htm">
+          <CardContent>
+            <Card.Img variant="top" src={Chicago} width='100px' height='160px' />
+            <Card.Body>
+              <Card.Title>New York</Card.Title>
+              <Card.Text>
+              New York is a city made up of five boroughs: 
+              Manhattan, Brooklyn, Queens, the Bronx, and Staten Island. 
+              Most tourist attractions are in Manhattan, 
+              with a few scattered across Brooklyn, Queens, and the Bronx. 
+              
+              </Card.Text>
+              <Typography>Know More!</Typography>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card >
+          <CardActionArea href="https://www.travelandleisure.com/travel-guide/san-francisco">
+          <CardContent>
+            <Card.Img variant="top" src={newOrleans} width='100px' height='160px' />
+            <Card.Body>
+              <Card.Title>San Francisco</Card.Title>
+              <Card.Text>
+              San Francisco is home to Golden Gate Park, 
+              which is the third most visited park in the U.S. 
+              and spans 1,017 acres. 
+              </Card.Text>
+              <Typography>Know More!</Typography>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+        </CardDeck>
+      </div>
+    </div>
   );
 }
 
